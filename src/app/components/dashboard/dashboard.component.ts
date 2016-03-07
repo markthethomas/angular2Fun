1205238abf70c94616d59550209ef50b475e1d68
Mark Thomas <markthethomas@gmail.com>
2016-03-07 11:03:27 -0800

    feature: create basic dashboard component

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'dashboard',
+  template: `<div>
+    <h2>this is the dashboard!</h2>
+  </div>`
+})
+
+export default class Dashboard {
+  constructor() {}
+}
